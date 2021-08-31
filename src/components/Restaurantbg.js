1a50590418e2ab8c9266cdfb9e447bf0f9572fb0
Greg <greg_markarian@hotmail.com>
2021-08-31 11:26:16 +0300

    background Picture with some text

---
+++ src/components/Restaurantbg.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Restaurantbg() {
+    return (
+      <div>
+        <div className={"restaurant"}>
+          <p className="text-5xl text-center text-white transform translate-y-16">
+            The Mama Restaurant
+          </p>
+          <p className="text-center text-white transform translate-y-20">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+      </div>
+    );
+}
+
+export default Restaurantbg
