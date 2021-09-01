9a325975ee883f871d10103312f5e90868e10c29
Greg <greg_markarian@hotmail.com>
2021-09-01 17:41:40 +0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import {
   FaApple,
   FaPlus,
 } from "react-icons/fa";
-import { IconContext,  } from "react-icons/lib";
+import { IconContext } from "react-icons/lib";
 
 function Footer(props) {
   return (
@@ -112,7 +112,7 @@ function Footer(props) {
             </li>
           </IconContext.Provider>
 
-          <li className="mt-4">
+          <li className="mt-10">
             <a href="#" className="text-black mt-6 font-bold">
               Download App
             </a>
@@ -132,9 +132,6 @@ function Footer(props) {
           </li>
         </ul>
       </div>
-
-
-
 
       {/* <div class="flex justify-center ">
         <div class="flex-none w-32 h-32 mr-4">
@@ -159,9 +156,28 @@ function Footer(props) {
         </div>
       </div> */}
 
+      {/* <div class="flex justify-center ">
+        <div class="flex-none w-32 h-32 mr-4">
+          <img
+            src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg"
+            alt=""
+            className="w-40 rounded-lg col-span-1"
+          />
+        </div>
+        <div class="flex-shrink w-30 h-30">
+          <p className="font-bold">Margherita Pizza + Choice of Desserts</p>
+          <p className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <p className="text-red-500 inline ml-3 font-bold">$10.99</p>
+          <p className="inline ml-4">*****(5.0)</p>
 
-
-      
+          <p className="text-gray-500 inline ml-5">5201 review</p>
+        </div>
+        <div class="flex-none w-20 h-20 flex justify-center items-center ">
+          <FaPlus className="text-red-600" />
+        </div>
+      </div> */}
     </div>
   );
 }
