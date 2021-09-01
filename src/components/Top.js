5271b03cd8d471c704fa5d6f5389e610e51eba1c
Greg <greg_markarian@hotmail.com>
2021-09-01 16:14:20 +0300

    navbar

--- src/components/Top.js
+++ src/components/Top.js
@@ -2,37 +2,49 @@ import React from "react";
 
 function Top() {
   return (
-    <div>
-      <section
-        className="float-left m-2 p-2">
-        <p className="text-red-300 inline font-bold">Food</p>
-        <p className="text-yellow-300 inline font-bold">Sense</p>
-        <ul className="inline-block">
-          <li className="text-gray-400 font-bold inline-block px-4">
-            <a href="#">Home</a>
-          </li>
-          <li className="text-gray-400 font-bold inline-block px-4">
-            <a href="#">About</a>
-          </li>
-          <li className="text-gray-400 font-bold inline-block px-4">
-            <a href="#">Restaurant</a>
-          </li>
-          <li className="text-gray-400 font-bold inline-block px-4">
-            <a href="#">Pages</a>
-          </li>
-          <li className="text-gray-400 font-bold inline-block px-4">
-            <a href="#">Services</a>
-          </li>
-          <li className="text-gray-400 font-bold inline-block px-4">
-            <a href="#">Contact</a>
-          </li>
-          <input type="text" name="" id="" className="inline-block " />
-          search
-        </ul>
-        <button className="text-white bg-red-400 rounded-full px-4 py-2">
-          Login.
-        </button>
-      </section>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3">
+        <div className="mt-4">
+          <p className="text-red-300  font-bold text-2xl inline">Food</p>
+          <p className="text-yellow-300 inline  font-bold text-2xl">Sense.</p>
+        </div>
+
+        <div className="mt-2">
+          <ul className="flex justify-center items-center">
+            <li className="text-gray-400 font-bold inline-block px-4 ">
+              <a href="#">Home</a>
+            </li>
+            <li className="text-gray-400 font-bold inline-block px-4">
+              <a href="#">About</a>
+            </li>
+            <li className="text-gray-400 font-bold inline-block px-4">
+              <a href="#">Restaurant</a>
+            </li>
+            <li className="text-gray-400 font-bold inline-block px-4">
+              <a href="#">Pages</a>
+            </li>
+            <li className="text-gray-400 font-bold inline-block px-4">
+              <a href="#">Services</a>
+            </li>
+            <li className="text-gray-400 font-bold inline-block px-4">
+              <a href="#">Contact</a>
+            </li>
+
+            <div className="flex ">
+              <input
+                placeholder="Search Here..."
+                type="text"
+                name=""
+                id=""
+                className="text-center rounded-lg focus:outline-none"
+              />
+              <button className="text-white bg-red-400 rounded-full px-4 py-2 items-center ">
+                Login.
+              </button>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
