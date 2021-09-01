5afe798f73f9909da81b4d30fcd7f3fbc14c63fd
Greg <greg_markarian@hotmail.com>
2021-09-01 16:19:04 +0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,87 +1,94 @@
 import React from "react";
-import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import { IconContext } from "react-icons/lib";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaGooglePlay,
+  FaApple,
+  FaPlus,
+} from "react-icons/fa";
+import { IconContext,  } from "react-icons/lib";
 
 function Footer(props) {
   return (
     <div>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-4 ">
         <ul className="mt-6 m-4 p-4">
-          <li>
+          <li className="mb-8">
             <a href="#" className="font-bold">
               Why Us ?
             </a>
           </li>
-          <li>
-            <a href="#" className="text-gray-500 ">
+          <li className="mb-3">
+            <a href="#" className="text-gray-500">
               Responsive
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Engagement
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Scale
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Watch the demo
             </a>
           </li>
         </ul>
         <ul className="mt-6 m-4 p-4">
-          <li>
+          <li className="mb-8">
             <a href="#" className="font-bold">
               Resources
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 ">
               Responsive
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Engagement
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Scale
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Watch the demo
             </a>
           </li>
         </ul>
         <ul className="mt-6 m-4 p-4">
-          <li>
+          <li className="mb-8">
             <a href="#" className="font-bold">
               Company
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 ">
               Responsive
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Engagement
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Scale
             </a>
           </li>
-          <li>
+          <li className="mb-3">
             <a href="#" className="text-gray-500 mt-6">
               Watch the demo
             </a>
@@ -89,7 +96,7 @@ function Footer(props) {
         </ul>
 
         <ul className="mt-6 m-4 p-4">
-          <li>
+          <li className="mb-4">
             <a href="#" className="font-bold">
               Our Social Media
             </a>
@@ -105,23 +112,56 @@ function Footer(props) {
             </li>
           </IconContext.Provider>
 
-          <li className="mt-6">
+          <li className="mt-4">
             <a href="#" className="text-black mt-6 font-bold">
               Download App
             </a>
           </li>
           <li>
             <div className="grid grid-cols-2 mr-8">
-              <button className="bg-black text-white rounded-full px-2 py-1 inline-block">
+              <FaGooglePlay className="mt-4" />
+              <FaApple className="mt-4" />
+
+              {/* <button className="bg-black text-white rounded-full px-2 py-1 inline-block">
                 Google Play
               </button>
               <button className="bg-black text-white rounded-full px-2 py-1 inline-block">
                 Apple Store
-              </button>
+              </button> */}
             </div>
           </li>
         </ul>
       </div>
+
+
+
+
+      {/* <div class="flex justify-center ">
+        <div class="flex-none w-32 h-32 mr-4">
+          <img
+            src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg"
+            alt=""
+            className="w-40 rounded-lg col-span-1"
+          />
+        </div>
+        <div class="flex-shrink w-30 h-30">
+          <p className="font-bold">Margherita Pizza + Choice of Desserts</p>
+          <p className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <p className="text-red-500 inline ml-3 font-bold">$10.99</p>
+          <p className="inline ml-4">*****(5.0)</p>
+
+          <p className="text-gray-500 inline ml-5">5201 review</p>
+        </div>
+        <div class="flex-none w-20 h-20 flex justify-center items-center ">
+          <FaPlus className="text-red-600" />
+        </div>
+      </div> */}
+
+
+
+      
     </div>
   );
 }
