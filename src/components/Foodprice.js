3e23fd9ec07316ffb1c9e79215d8989c890164c2
Greg <greg_markarian@hotmail.com>
2021-09-01 17:41:22 +0300

    Food price and some info

--- src/components/Foodprice.js
+++ src/components/Foodprice.js
@@ -3,7 +3,7 @@ import { FaPlus } from "react-icons/fa";
 
 function Foodprice() {
   return (
-    <div className="container mx-auto mt-4 flex bg-white rounded-lg">
+    <div className="container mx-auto mt-4 flex bg-white rounded-lg ">
       <div className="grid grid-cols-4 p-4 rounded-lg">
         <img
           src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg"
@@ -11,13 +11,13 @@ function Foodprice() {
           className="w-40 rounded-lg col-span-1"
         />
 
-        <div className="col-span-2 ml-2">
+        <div className="col-span-2 ml-2 ">
           <p className="font-bold">Margherita Pizza + Choice of Desserts</p>
           <p className="text-gray-500">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
           <p className="text-red-500 inline ml-3 font-bold">$10.99</p>
-          <p className="inline ml-4">*****(5.0)</p>
+          <p className="inline ml-4 ">*****(5.0)</p>
 
           <p className="text-gray-500 inline ml-5">5201 review</p>
         </div>
