48f0a7d16d9e3bb85488e852f8156d5a4c55cf3b
Greg <Greg_markarian@hotmail.com>
2021-09-02 16:39:40 +0300

    Food Choice Wiith Old And New Prices

--- src/components/Foodchoice.js
+++ src/components/Foodchoice.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function Foodchoice() {
+function Foodchoice(props) {
   return (
-    <div className = "container mx-auto">
+    <div className="w-3/2">
       <div className="ml-4 mr-4 w-auto">
         <div className="mt-5 rounded-lg bg-red-100 p-4">
-          <p className="font-bold mt-4">Cheese Stuffed Burgers</p>
-          <p className="text-gray-500 mt-4 mb-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <p className="font-bold mt-4">{props.title}</p>
+
+          <p className="text-gray-500 mt-4 mb-4">{props.subtitle}</p>
+          <p className="text-red-500 inline ">{props.newprice}</p>
+          <p className="inline text-red-400 line-through ml-2">
+            {props.oldprice}
           </p>
-          <p className="text-red-500 inline ">$8.99</p>
-          <p className="inline text-red-400 line-through ml-2">9.99</p>
           <button className="text-white bg-red-400 rounded-full px-2 y-4 ml-5 mb-4">
-            Order Now
+            {props.button}
           </button>
           <img
             className="rounded-lg"
